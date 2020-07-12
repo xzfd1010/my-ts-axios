bc6aed004cfad8f031dab78f159085a9c7974dc9
zhongcheng <zhongcheng@bytedance.com>
2020-07-12 14:05:16 +0800

    feat: 支持url的params

---
+++ src/helpers/url.ts
@@ -0,0 +1,55 @@
+import { isDate, isObject } from './util'
+
+function encode(val: string): string {
+  return encodeURIComponent(val)
+    .replace(/%40/g, '@')
+    .replace(/%3A/gi, ':')
+    .replace(/%24/g, '$')
+    .replace(/%2C/gi, ',')
+    .replace(/%20/gi, '+') // 空格变为'+'号
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/gi, ']')
+}
+
+// 将params中的参数拼接到url上
+export function buildURL(url: string, params?: any): string {
+  if (!params) {
+    return url
+  }
+  // 键值对数组
+  const parts: string[] = []
+  Object.keys(params).forEach(key => {
+    const val = params[key] // 当前key对应的value
+    if (val === null || val === undefined) {
+      // 跳到下次循环
+      return
+    }
+    // 值统一为数组
+    let values = []
+    if (Array.isArray(val)) {
+      values = val
+      key += '[]' // 标识目前的value是数组，符合url的规范 foo:['foo','bar'] 对应的url 应为 foo[]=foo&foo[]=bar
+    } else {
+      values = [val]
+    }
+    // values处理为数组
+    values.forEach(val => {
+      if (isDate(val)) {
+        val = val.toISOString()
+      } else if (isObject(val)) {
+        val = JSON.stringify(val)
+      }
+      parts.push(`${encode(key)}=${encode(val)}`)
+    })
+  })
+
+  let serializedParams = parts.join('&')
+  if (serializedParams) {
+    const markIndex = url.indexOf('#')
+    if (markIndex !== -1) {
+      url = url.slice(0, markIndex)
+    }
+    url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams
+  }
+  return url
+}
---
+++ src/helpers/util.ts
@@ -0,0 +1,10 @@
+const toString = Object.prototype.toString
+
+// 谓词保护
+export function isDate(val: any): val is Date {
+  return toString.call(val) === '[object Date]'
+}
+
+export function isObject(val: any): val is Object {
+  return val !== null && typeof val === 'object'
+}
