f395bb68f2dd74ba85b43a07e46535b5eae17ffa
zhongcheng <zhongcheng@bytedance.com>
2020-07-14 10:05:55 +0800

    feat: 处理请求body

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -5,6 +5,12 @@ export function isDate(val: any): val is Date {
   return toString.call(val) === '[object Date]'
 }
 
-export function isObject(val: any): val is Object {
-  return val !== null && typeof val === 'object'
+// 会把formData等类型也认为是object
+// export function isObject(val: any): val is Object {
+//   return val !== null && typeof val === 'object'
+// }
+
+// 普通对象判断方法
+export function isPlainObject(val: any): val is Object {
+  return toString.call(val) === '[object Object]'
 }
